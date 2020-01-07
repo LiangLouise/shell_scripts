96907eea6f3c24bfc97ab415c46d702104c292a8
LiangLouise <liangroy@yahoo.com>
2020-01-07 00:04:03 -0500

    Add a script to help automate install mysql on ubuntu server

---
+++ install_mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MYSQL_CNF="/etc/mysql/my.cnf"
+
+function set_config(){
+    sudo sed -i "s/\($1 *= *\).*/\1$2/" $MYSQL_CNF
+}
+
+
+sudo apt update
+[ ! -e /usr/bin/expect ] && { apt install expect; }
+apt install mysql-server
+SECURE_MYSQL=$(expect -c "
+
+set timeout 10
+spawn mysql_secure_installation
+
+expect \"Press y|Y for Yes, any other key for No: \"
+send \"n\r\"
+expect \"Change the password for root ? ((Press y|Y for Yes, any other key for No) : \"
+send \"n\r\"
+expect \"Remove anonymous users? (Press y|Y for Yes, any other key for No) : \"
+send \"y\r\"
+expect \"Disallow root login remotely? (Press y|Y for Yes, any other key for No) : \"
+send \"y\r\"
+expect \"Remove test database and access to it? (Press y|Y for Yes, any other key for No) : \"
+send \"y\r\"
+expect \"Reload privilege tables now? (Press y|Y for Yes, any other key for No) : \"
+send \"y\r\"
+expect eof
+")
+
+if [ $SECURE_MYSQL -ne 0 ] || [ -w $MYSQL_CNF ] then
+	echo "Failed to install"
+	exit 1
+fi
+
+
+
